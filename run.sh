7c4a89053d53cd3cdafe95f974fccdf9415d72f0
owelinux <1132818999@qq.com>
2021-02-09 11:56:48 +0800

    fix bugs

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 hexo clean
-#hexo g
-#hexo d
-#git add -A
-#git commit -m "$1"
-#git commit -m "fix bugs"
-#git push origin master
-rm -rf .deploy_git
-hexo clean && hexo deploy
+hexo g
+hexo d
+git add -A
+git commit -m "$1"
+git commit -m "fix bugs"
+git push origin master
+#rm -rf .deploy_git
+#hexo clean && hexo deploy
+
